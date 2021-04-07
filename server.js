09c8e73bf8d8588d8d35acac1b123cba1c22e6ed
Chen Lin <CLin4@slb.com>
2021-04-07 15:42:09 -0700

    create a simple node appication

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.get('/', async (req,res)=> {
+  res.status(200).json("hello from chen");
+})
+
+app.listen(8080, ()=> {
+  console.log('server start at 8080...');
+})
